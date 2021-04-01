f93ef738c288cd8755b7bb59faad147e4a96e8bd
ThaTechMaestro <muse.pearls@gmail.com>
2021-04-01 08:51:25 +0100

    Initial Commit

---
+++ C_Programming_Getting_Started/unit1.2/x8.c
@@ -0,0 +1,19 @@
+// Print the x8 multiplication table in a specified format
+
+#include <stdio.h>
+
+int main(void) {
+    int first_value;
+    int second_value = 8;
+    int age;
+
+
+    for (first_value = 0; first_value<11; first_value++)
+    {
+      printf("%d*%d = %d\n", first_value, second_value, (first_value*second_value));
+
+    }
+
+
+    return(0);
+}
